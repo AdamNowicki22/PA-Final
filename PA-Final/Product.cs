41cf43535de84f0fa699d2abfff9cfdd8e6aa54d
Adam Nowicki <adamnow83@gmail.com>
2023-06-29 19:47:48 +0200

    shovel

---
+++ PA-Final/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PA_Final
+{
+    internal abstract class Product
+    {
+        protected int Id;
+        protected int idCounter = 1;
+        protected string BrandName;
+        protected int Price;
+        protected bool isForSale;
+
+    }
+}
---
+++ PA-Final/Shovels.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PA_Final
+{
+    internal class Shovels: Product
+    {
+
+        private string Size { get; set; }
+        private string Material { get; set; }
+        public Shovels(string brandName, int price, string size, string material)
+        {
+            Id = idCounter++;
+            BrandName = brandName;
+            Price = price;
+            if(!Enum.IsDefined(typeof(shovelSize), size))
+            {
+                throw new ArgumentException("Wrong input)");
+            }
+            Size = size;
+            if (!Enum.IsDefined(typeof(shovelMaterial), material))
+            {
+                throw new ArgumentException("Wrong input)");
+            }
+            Material = material;
+            isForSale = true;
+
+        }
+        enum shovelSize
+        {
+            Small,
+            Medium,
+            Large,
+        }
+
+
+        enum shovelMaterial
+        {
+            Aluminum,
+            Steel,
+        }
+
+    }
+
+ 
+}
